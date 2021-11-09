9c1be83a3711615c79160c1cd1a50ef66889f872
Joseph May <70770554+jdmay2@users.noreply.github.com>
2021-11-09 12:07:21 -0600

    Config Vars

--- ConnectionString.cs
+++ ConnectionString.cs
@@ -1,3 +1,5 @@
+using System;
+
 namespace mis321_pa4_api
 {
     public class ConnectionString
@@ -5,11 +7,11 @@ namespace mis321_pa4_api
         public string cs { get; set; }
         public ConnectionString()
         {
-            string server = "g84t6zfpijzwx08q.cbetxkdyhwsb.us-east-1.rds.amazonaws.com";
-            string database = "n06gdg8me5vq9ygp";
-            string port = "3306";
-            string userName = "peevb8rnbanrtyn2";
-            string password = "z5ui4y0wgjneigb0";
+            string server = Environment.GetEnvironmentVariable("server");
+            string database = Environment.GetEnvironmentVariable("database");
+            string port = Environment.GetEnvironmentVariable("port");
+            string userName = Environment.GetEnvironmentVariable("username");
+            string password = Environment.GetEnvironmentVariable("password");
             cs = $@"server = {server};user={userName};database={database};port={port};password={password};";
         }
     }
